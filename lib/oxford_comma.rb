6cb6c3d93f20ab1f9f922be2995f00c6a246e8d1
Judy McGoldrick <jjeanius@verizon.net>
2017-12-03 04:53:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,9 @@ def oxford_comma(array)
   if array.count <= 2
     array.join(" and ")
   elsif array.count >= 3
-   array.join(" , ")
-   last_word = array.last
-   array.pop
-   array.push("and "+last_word).join(", ")
-
+    array.join(" , ")
+    last_word = array.last
+    array.pop
+    array.push("and "+last_word).join(", ")
   end
-
 end
